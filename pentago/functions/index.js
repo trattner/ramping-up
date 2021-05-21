1033fe5cc282dfc788249a3c37ffe659616949d8
atratt <yo@atratt.com>
2021-05-20 19:48:53 -0500

    database kidna works

--- pentago/functions/index.js
+++ pentago/functions/index.js
@@ -1,15 +1,64 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
-const cors = require('cors')({origin: true});
+//const cors = require('cors')({origin: true});
 
+/*
 // dummy endpoint
 exports.helloWorld = functions.https.onRequest((request, response) => {
   cors(req, res, () => {
       functions.logger.info("Hello logs!", {structuredData: true});
       response.send("Hello from Firebase!");
     })
+});*/
+
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
+  response.set('Access-Control-Allow-Headers', '*');
+
+  if (request.method === 'OPTIONS') {
+    response.end();
+  } else {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase!");
+  }
 });
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addMessage3 = functions.https.onRequest( (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
+  res.set('Access-Control-Allow-Headers', '*');
+
+  if (req.method === 'OPTIONS') {
+    res.end();
+  } else {
+    try {
+      functions.logger.info(req);
+      /*
+      const root = admin.database().ref();
+      admin.database().ref("/test_message/" + req.counter).set(req.text);
+      res.json({result: 'cool'});*/
+    } catch(err) {
+      functions.logger.info(err.message);
+    }
+
+
+
+    /*
+    const original = req.query.text;
+    // Push the new message into Firestore using the Firebase Admin SDK.
+    const writeResult = await admin.firestore().collection('messages').add({original: original});
+    // Send back a message that we've successfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+    */
+  }
+});
+
+
 
 // Take the text parameter passed to this HTTP endpoint and insert it into
 // Firestore under the path /messages/:documentId/original
@@ -21,3 +70,21 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Send back a message that we've successfully written the message
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
+
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate((snap, context) => {
+      // Grab the current value of what was written to Firestore.
+      const original = snap.data().original;
+
+      // Access the parameter `{documentId}` with `context.params`
+      functions.logger.log('Uppercasing', context.params.documentId, original);
+
+      const uppercase = original.toUpperCase();
+
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to Firestore.
+      // Setting an 'uppercase' field in Firestore document returns a Promise.
+      return snap.ref.set({uppercase}, {merge: true});
+    });
